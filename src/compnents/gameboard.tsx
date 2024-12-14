f2e283dead78f0be2839425a3701046dc24ee8ab
efran7107 <efran7107@gmail.com>
2024-12-13 21:37:38 -0700

    add gameboard logic
    need to add gameboard logic for game pieces to fall

--- src/compnents/gameboard.tsx
+++ src/compnents/gameboard.tsx
@@ -3,6 +3,8 @@ import '../styles/gameboard.css'
 
 export const Gameboard = () => {
 	const {name, gameboard} = useUser()
+	const rowNums = [1,2,3,4,5,6,7]
+
 	return(
 		<div className="game-container">
 			<div className="names-cont">
@@ -10,6 +12,12 @@ export const Gameboard = () => {
 				<h3>CPU</h3>
 			</div>
 			<div className="gameboard">
+				{rowNums.map(rowNum => <p
+					className='row-selection'
+					key={rowNum} id={rowNum.toString()}
+				>
+					{rowNum}
+				</p>)}
 				{gameboard.map((slot) => <div className='slot' key={slot.id}></div>)}
 			</div>
 		</div>
